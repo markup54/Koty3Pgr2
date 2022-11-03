13fef1218234c3c7f8e9dff1a71ed7e25be9c61d
markup54 <marzenakupka@tlen.pl>
2022-11-03 12:03:34 +0100

    do zmiany tlo

--- app/src/main/java/com/example/kotyinformator/MainActivity.java
+++ app/src/main/java/com/example/kotyinformator/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.kotyinformator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import java.util.ArrayList;
 
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     public Button dalejButton;
     public Button wsteczButton;
     public ImageView imageView;
+    public EditText editText;
+    public Switch aSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,34 @@ public class MainActivity extends AppCompatActivity {
         dalejButton = findViewById(R.id.button2);
         wsteczButton = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
+        editText = findViewById(R.id.editNumber);
+        aSwitch = findViewById(R.id.switch1);
+
+        editText.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        try {
+                            indeks = Integer.parseInt(s.toString());
+
+                            if (indeks >= 0 && indeks < zdjecia.size())
+                                imageView.setImageResource(zdjecia.get(indeks - 1));
+                        } catch (Exception ignore) {
+
+                        }
+                    }
+                }
+        );
         dalejButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -43,6 +78,20 @@ public class MainActivity extends AppCompatActivity {
                         if(indeks<0)
                             indeks = zdjecia.size()-1;
                         wyswietlZdjecie(indeks);
+                    }
+                }
+        );
+        aSwitch.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked)
+                        {
+                            //zmienic tlo
+                        }
+                        else {
+                            //zmienic tlo
+                        }
                     }
                 }
         );
